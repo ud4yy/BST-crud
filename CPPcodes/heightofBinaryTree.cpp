934941639b499ceb234ddc90f46e65a40ebec568
ud4yy <155240229+ud4yy@users.noreply.github.com>
2024-08-08 11:54:06 +0530

    Create heightofBinaryTree.cpp

---
+++ CPPcodes/heightofBinaryTree.cpp
@@ -0,0 +1,29 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int heightOfBinaryTree(TreeNode<int> *root)
+{
+	// Write your code here.
+    if(root==NULL) return 0;
+
+    return 1+max(heightOfBinaryTree(root->left),heightOfBinaryTree(root->right));
+}
